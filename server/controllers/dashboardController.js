1057aecc51088199794709cb8a8dac9b3df9fc40
Berkay Derin <zewodi@gmail.com>
2024-05-01 12:41:04 +0300

    transactionları listeleme fonksiyonu yazıldı

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,11 +1,23 @@
+const Transaction = require('../models/Transaction')
+const mongoose = require('mongoose')
+
 exports.dashboard = async (req, res) => {
 	const locals = {
 		title: 'Panel | cashmate',
 		description: 'Welcome to the homepage'
 	}
 
-	res.render('dashboard/index', {
-		locals,
-		layout: '../views/layouts/dashboard'
-	})
+	try {
+		// sadece giriş yapan kullanıcının işlemlerini getir
+		const transactions = await Transaction.find({ userId: new mongoose.Types.ObjectId(req.user.id) }).sort({ date: -1 })
+
+		res.render('dashboard/index', {
+			firstName: req.user.firstName,
+			locals,
+			transactions,
+			layout: '../views/layouts/dashboard'
+		})
+	} catch (error) {
+		console.log(error)
+	}
 }
